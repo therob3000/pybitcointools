bc382261dafde5e1fe1dbc85b6d0bb7955bc10ae
wizard of ozzie <simcity4242@users.noreply.github.com>
2015-04-22 10:13:42 +1000

    Update main.py

--- bitcoin/main.py
+++ bitcoin/main.py
@@ -554,9 +554,6 @@ def ecdsa_recover(msg, sig):
 
 # A simple implementation of Pbkdf2 using stock python modules.
 # Modifications based on https://matt.ucc.asn.au/src/pbkdf2.py
-import hmac
-import struct
- 
 def pbkdf2(password, salt, iters, keylen, digestmod):
     """Run the PBKDF2 (Password-Based Key Derivation Function 2) algorithm
     and return the derived key. The arguments are:
@@ -567,7 +564,6 @@ def pbkdf2(password, salt, iters, keylen, digestmod):
     digestmod -- a cryptographic hash function: either a module
         supporting PEP 247, a hashlib constructor, or (in Python 3.4
         or later) the name of a hash function.
- 
     For example:
     >>> import hashlib
     >>> from binascii import hexlify, unhexlify
@@ -575,12 +571,12 @@ def pbkdf2(password, salt, iters, keylen, digestmod):
     >>> salt = unhexlify(b'1234567878563412')
     >>> hexlify(pbkdf2(password, salt, 500, 16, hashlib.sha1))
     b'9e8f1072bdf5ef042bd988c7da83e43b'"""
+    
     h = hmac.new(password, digestmod=digestmod)
     def prf(data):
         hm = h.copy()
         hm.update(data)
         return bytearray(hm.digest())
- 
     key = bytearray()
     i = 1
     while len(key) < keylen:
@@ -590,5 +586,4 @@ def pbkdf2(password, salt, iters, keylen, digestmod):
             T = bytearray(x ^ y for x, y in zip(T, U))
         key += T
         i += 1
- 
     return key[:keylen]
